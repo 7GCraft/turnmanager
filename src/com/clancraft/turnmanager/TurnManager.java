c26c03d55e2ab02119dc591ede6c0eb7d2e721cd
Phobez <abiaph@live.com>
2019-07-19 16:38:07 +0700

    Added onEnable and onDisable methods

--- src/com/clancraft/turnmanager/TurnManager.java
+++ src/com/clancraft/turnmanager/TurnManager.java
@@ -1,6 +1,24 @@
 package com.clancraft.turnmanager;
 
-public class TurnManager extends JavaPlugin
-{
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TurnManager extends JavaPlugin {
     
+    public void onEnable() {
+        PluginDescriptionFile pdfFile = new PluginDescriptionFile();
+        Logger logger = getLogger();
+
+        logger.info(pdfFile.getName() + " has been enabled! (v." + pdfFile.getVersion() + ")");
+    }
+
+    public void onDisable() {
+		PluginDescriptionFile pdfFile = getDescription();
+		Logger logger = getLogger();
+
+		logger.info(pdfFile.getName() + " has been disabled! (v." + pdfFile.getVersion() + ")");
+	}
 }
