b645290032e717a75f1b3c9ee637d0bab75afc10
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2019-08-23 08:47:41 -0700

    Refactored "cycle" switch statement

--- src/com/clancraft/turnmanager/TM.java
+++ src/com/clancraft/turnmanager/TM.java
@@ -20,43 +20,11 @@ public class TM implements CommandExecutor {
             Player player = (Player) sender;
 
             if (args.length > 0) {
-                 switch (args[0])
-                 {
-                     case "cycle":
-                         switch (args[1]) {
-                         	 case "list":
-                         		 player.sendMessage(TurnManager.pluginPrefix + cycle.getTurnSequence());
-                         		 break;
-                             case "add":
-                            	 if (cycle.addPlayer(args[2])) {
-                            		 player.sendMessage(TurnManager.pluginPrefix + args[2] + " has been added to the turn sequence.");
-                            	 } else {
-                            		 player.sendMessage(TurnManager.pluginPrefix + ChatColor.RED + args[2] + "could not be added to the turn sequence.");
-                            	 }
-                                 break;
-                             case "remove":
-                            	 if (cycle.removePlayer(args[2])) {
-                            		 player.sendMessage(TurnManager.pluginPrefix + args[2] + " has been removed from the turn sequence.");
-                            	 } else {
-                            		 player.sendMessage(TurnManager.pluginPrefix + ChatColor.RED + args[2] + "could not be removed from the turn sequence.");
-                            	 }
-                                 break;
-                             case "swap":
-                            	 if (cycle.swap(args[2], args[3])) {
-                            		 player.sendMessage(TurnManager.pluginPrefix + "Players have been swapped in the turn sequence.");
-                            	 } else {
-                            		 player.sendMessage(TurnManager.pluginPrefix + ChatColor.RED + "Players could not be swapped in the turn sequence.");
-                            	 }
-                                 break;
-                             case "next":
-                                 cycle.nextTurn();
-                                 break;
-                             default:
-                            	 player.sendMessage(TurnManager.pluginPrefix + ChatColor.RED + "Invalid argument.");
-                            	 break;
-                         }
-                         break;
-                 }
+                switch (args[0]) {
+                    case "cycle":
+                        cycleHandler(player, args);
+                        break;
+                }
             } else {
                 player.sendMessage(TurnManager.pluginPrefix + "/tm requires an argument:");
                 player.sendMessage(TurnManager.pluginPrefix + "/tm [argument]");
@@ -66,4 +34,39 @@ public class TM implements CommandExecutor {
         return true;
     }
 
+    public boolean cycleHandler(Player player, String[] args) {
+        switch (args[1]) {
+            case "list":
+                player.sendMessage(TurnManager.pluginPrefix + cycle.getTurnSequence());
+                break;
+            case "add":
+                if (cycle.addPlayer(args[2])) {
+                    player.sendMessage(TurnManager.pluginPrefix + args[2] + " has been added to the turn sequence.");
+                } else {
+                    player.sendMessage(TurnManager.pluginPrefix + ChatColor.RED + args[2] + "could not be added to the turn sequence.");
+                }
+                break;
+            case "remove":
+                if (cycle.removePlayer(args[2])) {
+                    player.sendMessage(TurnManager.pluginPrefix + args[2] + " has been removed from the turn sequence.");
+                } else {
+                    player.sendMessage(TurnManager.pluginPrefix + ChatColor.RED + args[2] + "could not be removed from the turn sequence.");
+                }
+                break;
+            case "swap":
+                if (cycle.swap(args[2], args[3])) {
+                    player.sendMessage(TurnManager.pluginPrefix + "Players have been swapped in the turn sequence.");
+                } else {
+                    player.sendMessage(TurnManager.pluginPrefix + ChatColor.RED + "Players could not be swapped in the turn sequence.");
+                }
+                break;
+            case "next":
+                cycle.nextTurn();
+                break;
+            default:
+                player.sendMessage(TurnManager.pluginPrefix + ChatColor.RED + "Invalid argument.");
+                break;
+        }
+        return true;
+    }
 }
