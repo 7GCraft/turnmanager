4e94266d4122ccd642615c6490ff1cc5dba9078d
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2019-10-27 00:04:13 -0700

    Refactored MAX_OVERTIME_MINS as constant, added class header and method headers

--- src/com/clancraft/turnmanager/TurnTimer.java
+++ src/com/clancraft/turnmanager/TurnTimer.java
@@ -2,7 +2,13 @@ package com.clancraft.turnmanager;
 
 import org.bukkit.Bukkit;
 
+/**
+ * A class to create threads that function as a timer. The timer duration is
+ * configurable, and will keep alerting users after time is over. Timer thread 
+ * automatically stops (by default) 60 minutes after time is over.
+ */
 public class TurnTimer extends Thread {
+    private final int MAX_OVERTIME_MINS = 60;
     private final int NORMAL_INTERVAL = 5;
     private final int OVERTIME_INTERVAL = 1;
     private final int MIN_TO_MS = 60000;
@@ -10,10 +16,20 @@ public class TurnTimer extends Thread {
     private int minutesRemaining;
     private volatile boolean terminateNow = false;
 
+    /**
+     * Default constructor that accepts the duration of the timer.
+     * @param minute duration of the timer
+     */
     public TurnTimer(int minute) {
         minutesRemaining = minute;
     }
 
+    /**
+     * Executed when the thread is running. Keeps track of time remaining by
+     * decrementing variable value every NORMAL_INTERVAL minutes. Timer keeps
+     * running after time is up, reminding user every OVERTIME_INTERVAL, up to
+     * MAX_OVERTIME_MINS minutes.
+     */
     @Override
     public void run() {
         Bukkit.broadcastMessage(String.format(TMStrings.TIMER_INITIAL, minutesRemaining));
@@ -35,21 +51,24 @@ public class TurnTimer extends Thread {
                 }
                 
             } catch (Exception InterruptedException) {
-                System.err.println("My slumber is disturbed.");
+                // TODO print error message to logger.
             }
         }
 
-        while (!terminateNow && minutesRemaining < 60) {
+        while (!terminateNow && minutesRemaining < MAX_OVERTIME_MINS) {
             Bukkit.broadcastMessage(String.format(TMStrings.TIMER_OVERTIME, minutesRemaining));
             try {
                 Thread.sleep(OVERTIME_INTERVAL * MIN_TO_MS);
             } catch (Exception InterruptedException) {
-                System.err.println("My rest is disturbed.");
+                // TODO print error message to logger.
             }
             minutesRemaining += OVERTIME_INTERVAL;
         }
     }
 
+    /**
+     * Method to stop the execution of the thread.
+     */
     public void halt() {
         terminateNow = true;
     }
