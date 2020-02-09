9b82022028d3b2c01e806d23a460e87d44db3b6c
Phobez <abiaph@live.com>
2020-02-09 16:16:32 +0700

    Minor error fix.
    Added additional )-s.

--- src/com/clancraft/turnmanager/TMCommandHandler.java
+++ src/com/clancraft/turnmanager/TMCommandHandler.java
@@ -73,14 +73,14 @@ public class TMCommandHandler implements CommandExecutor {
             }
             break;
         case "pause":
-            if (player.hasPermission(TMConstants.TIMER_PAUSE_PERMISSION) {
+            if (player.hasPermission(TMConstants.TIMER_PAUSE_PERMISSION)) {
                 TurnManager.turn.pauseTimer();
             } else {
                 player.sendMessage(TMConstants.NO_PERMISSION_ERROR);
             }
             break;
         case "resume":
-        if (player.hasPermission(TMConstants.TIMER_RESUME_PERMISSION) {
+        if (player.hasPermission(TMConstants.TIMER_RESUME_PERMISSION)) {
                 TurnManager.turn.resumeTimer();
             } else {
                 player.sendMessage(TMConstants.NO_PERMISSION_ERROR);
