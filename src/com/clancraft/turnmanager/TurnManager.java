f951e090e0efb5fb82f76fa99bdebcf09ad489d1
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2020-03-21 09:45:29 -0700

    Added code to load shield data and write shield data

--- src/com/clancraft/turnmanager/TurnManager.java
+++ src/com/clancraft/turnmanager/TurnManager.java
@@ -12,62 +12,66 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Main driver class of the TurnManager plugin
- * TODO method Javadocs
  */
+// TODO method Javadocs
 public class TurnManager extends JavaPlugin {
     protected static Cycle cycle;
     protected static Turn turn;
     protected static Teleport teleport;
     protected static Shield shield;
-    
+
     private File shieldsConfigFile;
     private FileConfiguration shieldsConfig;
-	
+
     public void onEnable() {
         PluginDescriptionFile pdfFile = getDescription();
         Logger logger = getLogger();
-        
+
         createShieldsConfig();
-        
+
         registerCommands();
-        
+
         cycle = new Cycle();
         turn = new Turn();
         teleport = new Teleport();
         shield = new Shield();
 
+        shield.loadShieldData(this);
+
         logger.info(pdfFile.getName() + " has been enabled! (v." + pdfFile.getVersion() + ")");
     }
 
     public void onDisable() {
-		PluginDescriptionFile pdfFile = getDescription();
-		Logger logger = getLogger();
+        PluginDescriptionFile pdfFile = getDescription();
+        Logger logger = getLogger();
 
-		logger.info(pdfFile.getName() + " has been disabled! (v." + pdfFile.getVersion() + ")");
+        shield.writeShieldData(this);
+
+        logger.info(pdfFile.getName() + " has been disabled! (v." + pdfFile.getVersion() + ")");
     }
-    
+
     private void registerCommands() {
         getCommand("tm").setExecutor(new TMCommandHandler());
     }
-    
+
     private void createShieldsConfig() {
-    	shieldsConfigFile = new File(getDataFolder(), TMConstants.SHIELDS_CONFIG_FILE_NAME);
-    	
-    	if (!shieldsConfigFile.exists()) {
-    		shieldsConfigFile.getParentFile().mkdirs();
-    		saveResource(TMConstants.SHIELDS_CONFIG_FILE_NAME, false);
-    	}
-    	
-    	shieldsConfig = new YamlConfiguration();
-    	
-    	try {
-    		shieldsConfig.load(shieldsConfigFile);
-    	} catch (IOException | InvalidConfigurationException e) {
-    		e.printStackTrace();
-    	}
+        shieldsConfigFile = new File(getDataFolder(), TMConstants.SHIELDS_CONFIG_FILE_NAME);
+
+        if (!shieldsConfigFile.exists()) {
+            shieldsConfigFile.getParentFile().mkdirs();
+            saveResource(TMConstants.SHIELDS_CONFIG_FILE_NAME, false);
+        }
+
+        shieldsConfig = new YamlConfiguration();
+
+        try {
+            shieldsConfig.load(shieldsConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
     }
-    
+
     public FileConfiguration getShieldsConfig() {
-    	return this.shieldsConfig;
+        return this.shieldsConfig;
     }
 }
