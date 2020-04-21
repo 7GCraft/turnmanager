150d3630ac05770aaa33b426ad88d8186fdb23a7
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2020-04-21 15:20:09 -0700

    Initial commit

---
+++ src/com/clancraft/turnmanager/PositionChecker.java
@@ -0,0 +1,63 @@
+package com.clancraft.turnmanager;
+
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class PositionChecker implements Runnable {
+    public final Double SHIELD_RADIUS = 65.00;
+
+    Cycle cycle; // to get current player
+    HashMap<String, PlayerCoordinate> coordinateMap;
+
+    public PositionChecker(Cycle cycle) {
+        this.setCycle(cycle);
+        coordinateMap = new HashMap<>();
+
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            Location loc = player.getLocation();
+            coordinateMap.put(player.getName(), new PlayerCoordinate(loc.getX(), loc.getY(), loc.getZ()));
+        });
+    }
+
+    public void setCycle(Cycle cycle) {
+        this.cycle = cycle;
+    }
+
+    /**
+     * Scan everyone's position every 5 seconds. If a player is within 65m of the
+     * active player, their position is reverted back. Maybe can also send an error
+     * message: "You approached a shielded area" TODO
+     */
+    @Override
+    public void run() {
+        Player currPlayer = Bukkit.getPlayer(cycle.currentPlayer()); // TODO deprecated method
+
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            Location loc = player.getLocation();
+            Double distSqr = Math.pow(loc.getX() - currPlayer.getLocation().getX(), 2)
+                    + Math.pow(loc.getY() - currPlayer.getLocation().getY(), 2);
+
+            if (distSqr < Math.pow(SHIELD_RADIUS, 2)) {
+                loc.setX(coordinateMap.get(player.getName()).x);
+                loc.setY(coordinateMap.get(player.getName()).y);
+                loc.setZ(coordinateMap.get(player.getName()).z);
+            } else {
+                coordinateMap.put(player.getName(), new PlayerCoordinate(loc.getX(), loc.getY(), loc.getZ()));
+            }
+        });
+        // TODO make sure the scheduler repeats the task every 5 seconds
+    }
+
+    class PlayerCoordinate {
+        public double x, y, z;
+
+        public PlayerCoordinate(double x, double y, double z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
