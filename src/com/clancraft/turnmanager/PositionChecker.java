ebf0ddaa94ea6d6109e31e1154f76b7e8f5f31e9
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2020-05-18 22:11:40 +0700

    Replaced deprecated method

--- src/com/clancraft/turnmanager/PositionChecker.java
+++ src/com/clancraft/turnmanager/PositionChecker.java
@@ -27,7 +27,20 @@ public class PositionChecker implements Runnable {
      */
     @Override
     public void run() {
-        Player currPlayer = Bukkit.getPlayer(TurnManager.cycle.currentPlayer()); // TODO deprecated method
+        Player currPlayer = null;
+        Iterator<? extends Player> playerIter = Bukkit.getOnlinePlayers().iterator();
+        while (playerIter.hasNext()) {
+            Player p = playerIter.next();
+            if (p.getName().equals(TurnManager.cycle.currentPlayer())) {
+                currPlayer = p;
+            }
+        }
+
+        if (currPlayer == null) {
+            // TODO send Bukkit error log
+            // Log.e("Fatal error! Current Player object can't be found!");
+            return;
+        }
 
         Bukkit.getOnlinePlayers().forEach(player -> {
             Location loc = player.getLocation();
