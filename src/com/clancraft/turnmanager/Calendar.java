362b34af8146936aeb07e83bfcfd1f6c632b90a0
Phobez <36761666+Phobez@users.noreply.github.com>
2020-05-18 16:57:58 +0700

    Implemented all functionality on the Calendar side except for automatic movement.

--- src/com/clancraft/turnmanager/Calendar.java
+++ src/com/clancraft/turnmanager/Calendar.java
@@ -14,21 +14,21 @@ import org.bukkit.plugin.java.JavaPlugin;
  * A class to handle Date functionality.
  */
 public class Calendar {
-	
-	private HashMap<String, Date> playerDates;
-	private Date worldDate;
-	
-	private File calendarConfigFile;
-	private FileConfiguration calendarConfig;
-	
-	/**
-	 * Default constructor. Initialises fields.
-	 * @param plugin reference to plugin
-	 */
-	public Calendar(JavaPlugin plugin) {
-	    playerDates = new HashMap<>();
-		
-	    calendarConfigFile = new File(plugin.getDataFolder(), TMConstants.CALENDAR_CONFIG_FILE_NAME);
+    
+    private HashMap<String, Date> playerDates;
+    private Date worldDate;
+    
+    private File calendarConfigFile;
+    private FileConfiguration calendarConfig;
+    
+    /**
+     * Default constructor. Initialises fields.
+     * @param plugin reference to plugin
+     */
+    public Calendar(JavaPlugin plugin) {
+        playerDates = new HashMap<>();
+        
+        calendarConfigFile = new File(plugin.getDataFolder(), TMConstants.CALENDAR_CONFIG_FILE_NAME);
         
         if (!calendarConfigFile.exists()) {
             calendarConfigFile.getParentFile().mkdirs();
@@ -36,18 +36,18 @@ public class Calendar {
         }
         
         calendarConfig = YamlConfiguration.loadConfiguration(calendarConfigFile);
-	}
-	
-	public void loadCalendarData() {
-	    String worldDateString = this.getCalendarConfig().getString("world-date");
-	    String[] worldDateStringParts = worldDateString.split("-");
-	    int worldDay = Integer.parseInt(worldDateStringParts[0]);
+    }
+    
+    public void loadCalendarData() {
+        String worldDateString = this.getCalendarConfig().getString("world-date");
+        String[] worldDateStringParts = worldDateString.split("-");
+        int worldDay = Integer.parseInt(worldDateStringParts[0]);
         Date.Month worldMonth = Date.Month.values()[Integer.parseInt(worldDateStringParts[1])];
         int worldYear = Integer.parseInt(worldDateStringParts[2]);
         
         worldDate = new Date(worldDay, worldMonth, worldYear, false);
-	    
-	    List<String> playerList = this.getCalendarConfig().getStringList("playerlist");
+        
+        List<String> playerList = this.getCalendarConfig().getStringList("playerlist");
         
         playerList.forEach(playerName -> {
             String dateString = this.getCalendarConfig().getString("dates." + playerName + ".date");
@@ -60,46 +60,134 @@ public class Calendar {
 
             playerDates.put(playerName, date);
         });
-	}
-	
-	/**
-	 * Advances player's date by specified number of days.
-	 * 
-	 * @param playerName   player's date to be advanced
-	 * @param daysToAdd    days to advance
-	 * @return
-	 */
-	public boolean addDate(String playerName, int daysToAdd) {
-	    Date playerDate = playerDates.get(playerName);
-	    
-	    if (playerDate.getIsSynced()) {
-	        return false;
-	    }
-	    
-	    playerDate.add(daysToAdd);
-	    
-	    return true;
-	}
-	
-	/**
-	 * Advances world date and all synced dates.
-	 * @param daysToAdd    days to advance
-	 * @return
-	 */
-	public boolean addWorldDate(int daysToAdd) {
-	    worldDate.add(daysToAdd);
-	    
-	    // advance all synced player dates
-	    playerDates.forEach((playerName, playerDate) -> {
-	        if (playerDate.getIsSynced()) {
-	            playerDate.setDate(worldDate.getDay(), worldDate.getMonth(), worldDate.getYear());
-	        }
-	    });
-	    
-	    return true;
-	}
-	
-	private FileConfiguration getCalendarConfig() {
-	    return calendarConfig;
-	}
+    }
+    
+    /**
+     * Advances player's date by specified number of days.
+     * 
+     * @param playerName   player's date to be advanced
+     * @param daysToAdd    days to advance
+     * @return
+     */
+    public boolean addPlayerDate(String playerName, int daysToAdd) {
+        Date playerDate = playerDates.get(playerName);
+        
+        if (playerDate.getIsSynced()) {
+            return false;
+        }
+        
+        playerDate.add(daysToAdd);
+        
+        return true;
+    }
+    
+    /**
+     * Advances world date and all synced dates.
+     * @param daysToAdd    days to advance
+     * @return
+     */
+    public boolean addWorldDate(int daysToAdd) {
+        worldDate.add(daysToAdd);
+        
+        // advance all synced player dates
+        syncPlayerDates();
+        
+        return true;
+    }
+
+    /**
+     * Sets a specified player's date.
+     * @param playerName
+     * @param day
+     * @param month
+     * @param year
+     * @return
+     */
+    public boolean setPlayerDate(String playerName, int day, int month, int year) {
+        Date playerDate = playerDates.get(playerName);
+        
+        if (playerDate.getIsSynced()) {
+            return false;
+        }
+        
+        playerDate.setDate(day, Date.Month.values()[month], year);
+        
+        return true;
+    }
+
+    /**
+     * Sets the world date and all synced dates.
+     * @param day
+     * @param month
+     * @param year
+     * @return
+     */
+    public boolean setWorldDate(int day, int month, int year) {
+        worldDate.setDate(day, Date.Month.values()[month], year);
+
+        // set all synced player dates
+        syncPlayerDates();
+
+        return true;
+    }
+
+    /**
+     * Syncs a specified player's date to the world date.
+     * @param playerName
+     * @return
+     */
+    public boolean sync(String playerName) {
+        Date playerDate = playerDates.get(playerName);
+
+        playerDate.setIsSynced(true);
+
+        return true;
+    }
+
+    /**
+     * Unsyncs a specified player's date to the world date.
+     * @param playerName
+     * @return
+     */
+    public boolean unsync(String playerName) {
+        Date playerDate = playerDates.get(playerName);
+
+        playerDate.setIsSynced(false);
+
+        return true;
+    }
+
+    /**
+     * Returns a specified player's date.
+     * @param playerName
+     * @return  specified player's current date
+     */
+    public String getPlayerDate(String playerName) {
+        Date playerDate = playerDates.get(playerName);
+
+        return playerDate.getDate();
+    }
+
+    /**
+     * Returns the world date.
+     * @return  current world date
+     */
+    public String getWorldDate() {
+        return worldDate.getDate();
+    }
+
+    /**
+     * Syncs all synced player dates to the world date.
+     */
+    private void syncPlayerDates() {
+        playerDates.forEach((playerName, playerDate) -> {
+            if (playerDate.getIsSynced()) {
+                playerDate.setDate(worldDate.getDay(), worldDate.getMonth(), worldDate.getYear());
+            }
+        });
+    }
+    
+    private FileConfiguration getCalendarConfig() {
+        return calendarConfig;
+    }
 }
